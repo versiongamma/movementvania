34e4e0a0c9917ed637062c5f874a3c14ac78eda9
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-08-24 18:17:26 +1200

    Fix player clipping into floor when landing

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -11,31 +11,28 @@ public class PlayerMovement : MonoBehaviour {
     private bool grounded;
     private bool usedDoubleJump;
 
-    // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update() {
-                                  
+
+        // Movement Handling                          
         float targetHorizontalV = Input.GetAxis("Horizontal") * movementSpeed;
-                                  // Digital input doesn't have any smoothing, so this lerp smooths out  
-                                  // the horizontal acceleration based on the target velocity
+        // Digital input doesn't have any smoothing, so this lerp smooths out the horizontal acceleration based on the target velocity
         float horizontalV = Mathf.Lerp(rb.velocity.x, targetHorizontalV, Mathf.Abs(rb.velocity.x) < Mathf.Abs(targetHorizontalV) ? 0.05f : 0.2f);
         float verticalV = rb.velocity.y;
 
-        // Needs grounding later
+        // Jump Handling
         if(Input.GetKeyDown(KeyCode.Space) && (grounded || !usedDoubleJump)) {
             verticalV = jumpPower;
             if (!grounded && !usedDoubleJump) { usedDoubleJump = true; }
         }
 
-        Debug.Log(grounded);
-
         rb.velocity = new Vector3(horizontalV, Mathf.Clamp(verticalV, -maxFallSpeed, float.MaxValue), 0);
     }
 
+    // Grounding Triggers
     private void OnTriggerStay2D(Collider2D other) {
         grounded = true;
         usedDoubleJump = false;
