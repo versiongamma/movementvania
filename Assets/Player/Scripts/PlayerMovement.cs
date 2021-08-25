12d60d4269ba10e482f90119eb6eddd599a6cb27
Julia Borlase <zqj5293@aut.ac.nz>
2021-08-25 12:56:02 +1200

    Basic dash added
    Character will now dash when the 'Z' key is pressed (key can be changed later).

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -2,28 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
-    Rigidbody2D rigidbody;
+    Rigidbody2D rb;
     [SerializeField] private float movmentSpeed = 6;
     [SerializeField] private float jumpPower = 10;
 
+    public float dashForce = 10f;
+    public float startDashTime = 0.5f;
+    private float currentDashTime;
+    private float dashDirection;
+    private bool isDashing;
+
     // Start is called before the first frame update
-    void Start() {
-        rigidbody = GetComponent<Rigidbody2D>();
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update(){
+        //horizontal and vertical movements
         float horizontalV = Input.GetAxis("Horizontal") * movmentSpeed;
-        float verticalV = rigidbody.velocity.y;
+        float verticalV = rb.velocity.y;
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        //basic movements if/else loop (incl double jumps and ground checks)
+        if (Input.GetKeyDown(KeyCode.Space)){
             verticalV += jumpPower;
         }
 
 
+        rb.velocity = new Vector3(horizontalV, verticalV, 0);
 
-        rigidbody.velocity = new Vector3(horizontalV, verticalV, 0);
+        //code for dashing
+        if (Input.GetKeyDown(KeyCode.Z) && horizontalV != 0){
+            isDashing = true;
+            currentDashTime = startDashTime;
+            rb.velocity = Vector2.zero;
+            dashDirection = (int)horizontalV;
+
+            if (isDashing)
+            {
+                rb.velocity = transform.right * dashDirection * dashForce;
+                currentDashTime -= Time.deltaTime;
+
+                if (currentDashTime <= 0)
+                {
+                    isDashing = false;
+                }
+            }
+        }
+        //code for dashing
     }
 }
