725b6def12c83c5b72a22155ca56ec6c2e24d97f
Julia Borlase <zqj5293@aut.ac.nz>
2021-09-02 09:51:52 +1200

    Finished - needs testings
    Buttons now work, and game and control settings scenes now have a UI in them

--- Assets/ChangeScene.cs
+++ Assets/ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ChangeScene : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
