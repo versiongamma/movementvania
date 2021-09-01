91bc6187c66347702cfb0310a1f6acf5939544c4
Gamma <37131904+versiongamma@users.noreply.github.com>
2021-09-01 15:06:55 +1200

    Changed camera movement, and added door transition

---
+++ Assets/Scripts/Camera/CameraBoundsHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBoundsHandler : MonoBehaviour {
+
+    private CameraMovement cameraMovement;
+
+    public void SetCameraBounds(Camera camera) {
+        cameraMovement = camera.GetComponent<CameraMovement>();
+        float minX, minY, maxX, maxY;
+        Collider2D col = this.GetComponent<Collider2D>();
+
+        // Get the center of the camera bounds collider and add or subtract half the width (bounds.extents) to find
+        // the +/- x edges, then add/subtract half camera width (size * aspectRatio / 2) to find the edges of the 
+        // cameras position to stay within that bounding box
+        minX = col.bounds.center.x - col.bounds.extents.x + (camera.orthographicSize * camera.aspect);
+        maxX = col.bounds.center.x + col.bounds.extents.x - (camera.orthographicSize * camera.aspect);
+
+        minY = col.bounds.center.y - col.bounds.extents.y + camera.orthographicSize;
+        maxY = col.bounds.center.y + col.bounds.extents.y - camera.orthographicSize;
+
+        cameraMovement.Transition(minX, minY, maxX, maxY);
+    }
+}
--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
     [SerializeField] private GameObject player;
-    private float yOffset = 3.25f;
+    [SerializeField] private GameObject startingBounds;
+    private float cameraSmootingAmount = 0.4f;
+    private float roomTransitionSmoothingAmount = 28f;
 
-    // Start is called before the first frame update
+    protected float minX = float.MinValue;
+    protected float maxX = float.MaxValue;
+    protected float minY = float.MinValue;
+    protected float maxY = float.MaxValue;
+
+    private float newX, newY;
+
     void Start() {
-        
+        startingBounds.GetComponent<CameraBoundsHandler>().SetCameraBounds(this.GetComponent<Camera>());
+    }
+    
+    void FixedUpdate() {
+        newX = Mathf.Clamp(player.transform.position.x, minX, maxX); 
+        newY = Mathf.Clamp(player.transform.position.y + 6, minY, maxY); 
+
+
+                                                                                             // Invert camera smoothing so higher 
+                                                                                             // value means more smoothing
+        transform.position = Vector3.Slerp(transform.position, new Vector3(newX, newY, -10), 1 / cameraSmootingAmount * 10);
     }
 
-    // Update is called once per frame
-    void Update() {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOffset, -10);
+    public void Transition(float minX, float minY, float maxX, float maxY) {
+        float smoothing = cameraSmootingAmount; // Store the camera smoothing for later
+
+        // Increase the camera smoothing for the room transition, this makes the camera transitioning
+        // from one room to another more fluid looking, rather than having it snap to each room
+        cameraSmootingAmount = roomTransitionSmoothingAmount; 
+
+        this.minX = minX;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+
+        // Set the smoothing to be reset after .3 seconds
+        StartCoroutine(ResetSmoothing(smoothing, .3f));
+    }
+
+    // Resets the camera smoothing back to the default after a specified amount of time
+    private IEnumerator ResetSmoothing(float smoothing, float delta) {
+        yield return new WaitForSeconds(delta);
+        cameraSmootingAmount = smoothing;
     }
 }
