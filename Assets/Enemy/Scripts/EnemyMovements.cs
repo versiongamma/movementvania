49461b8c4a8c82432cac8af5aac95d8a7587645f
Julia Borlase <zqj5293@aut.ac.nz>
2021-09-05 14:42:28 +1200

    enemy created
    folder created and movements added

---
+++ Assets/Enemy/Scripts/EnemyMovements.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovements : MonoBehaviour
+{
+    public float direction = 1.5f;
+    public float speed = 2f;
+    private Vector2 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 v = startPosition;
+        v.x += direction * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
