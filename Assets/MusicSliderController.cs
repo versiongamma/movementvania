98ac97b4d68d459d3f9eb4b8cf5bc97205ebb9ec
Will <willelliottmr@gmail.com>
2021-10-06 22:08:09 +1300

    Implemented music slider

--- Assets/MusicSliderController.cs
+++ Assets/MusicSliderController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class SfxSliderController : MonoBehaviour
+public class MusicSliderController : MonoBehaviour
 {
     // Get the global audio mixer
     [SerializeField] AudioMixer mixer;
@@ -13,12 +13,12 @@ public class SfxSliderController : MonoBehaviour
     //Initialize with previously used sfx volume
     public void Start()
     {
-        slider.value = PlayerPrefs.GetFloat("SfxVolume");
+        slider.value = PlayerPrefs.GetFloat("MusicVolume");
     }
     //Function to translate slider value to volume for mixer group
     public void SetLevel(float SliderValue)
     {
-        mixer.SetFloat("SfxVolume", Mathf.Log10(SliderValue) * 20);
-        PlayerPrefs.SetFloat("SfxVolume", slider.value);
+        mixer.SetFloat("MusicVolume", Mathf.Log10(SliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", slider.value);
     }
 }
