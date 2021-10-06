92ada70008528d016632e8242eb7aaeb0eacd2ec
Will <willelliottmr@gmail.com>
2021-10-06 22:01:02 +1300

    Slider now controls sfx volume

--- Assets/SfxSliderController.cs
+++ Assets/SfxSliderController.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SfxSliderController : MonoBehaviour
 {
     // Get the global audio mixer
     [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider slider;
 
-
+    //Initialize with previously used sfx volume
+    public void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("SfxVolume", 0.75f);
+    }
     //Function to translate slider value to volume for mixer group
     public void SetLevel(float SliderValue)
     {
