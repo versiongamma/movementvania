317c7755404324d0f579f75fc62076b119ba52f1
Will <willelliottmr@gmail.com>
2021-10-19 14:26:41 +1300

    Completed unit test

--- Assets/Tests/VoumeSettingsTest.cs
+++ Assets/Tests/VoumeSettingsTest.cs
@@ -1,15 +1,27 @@
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
+using UnityEditor;
 
-public class VoumeSettingsTest
+public class VoumeSettingsTest : MonoBehaviour
 {
+    GameObject sfxSliderGO;
+    Slider slider;
 
-    [Test]
-    public void VoumeSettingsTestSimplePasses() {
-        
+    [SetUp]
+    public void VolumeSettingsTestSetup() {
+        sfxSliderGO = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/UI/Prefabs/SfxSlider.prefab");
+        sfxSliderGO = GameObject.Instantiate(sfxSliderGO);
+        slider = sfxSliderGO.GetComponent<Slider>();
+    }
+
+    [UnityTest]
+    public IEnumerator VolumeSettingsTestWithEnumeratorPasses()
+    {
+        yield return new WaitForFixedUpdate();
+        Assert.AreEqual(slider.value, PlayerPrefs.GetFloat("SfxVolume"));
     }
 
 }
